dd06aff7ee92f4790008128b17b20e4b1a4dc755
Eirubedo <ekaputri.rubedo@gmail.com>
2024-08-02 18:32:07 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+title: 'AURORA',
+  description: 'Aku adalah sahabat rahasia kamu yang selalu siap dengerin curhatanmu. Aku juga punya banyak trik keren buat bikin kamu ngerasa lebih baik dan semangat lagi. Udah kayak punya guru kehidupan pribadi, tapi tanpa bikin kamu bete :)',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
