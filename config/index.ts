cb7e2b4ee2bdea789a346c4926c2faa43f74046c
Eirubedo <ekaputri.rubedo@gmail.com>
2024-08-12 17:50:53 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,7 @@ export const APP_INFO: AppInfo = {
 title: 'AURORA',
   description: 'Aku adalah sahabat rahasia kamu yang selalu siap dengerin curhatanmu. Aku juga punya banyak trik keren buat bikin kamu ngerasa lebih baik dan semangat lagi. Udah kayak punya guru kehidupan pribadi, tapi tanpa bikin kamu bete :)',
   copyright: '',
-  privacy_policy: '',
+  privacy_policy: 'Eka Putri Yulianti',
   default_language: 'zh-Hans',
 }
 
